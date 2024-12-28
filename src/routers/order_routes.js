b1777495fa8cec399ee72219b05db276d7cee894
ArielCatucuamba <davidcatudiaz22@gmail.com>
2024-12-28 11:02:43 -0500

    Ruta-verOrdenes

--- src/routers/order_routes.js
+++ src/routers/order_routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { createOrder, updateStateOrder } from '../controllers/orders_controller.js'
+import { createOrder, updateStateOrder,getAllOrders } from '../controllers/orders_controller.js'
 import { verificarAutenticacion } from '../middlewares/JWT.js'
 
 const router = Router()
 
 router.post('/orders/create', verificarAutenticacion, createOrder)
 router.patch('/orders/update/state/:id', verificarAutenticacion, updateStateOrder)
+router.get("/seeOrders",verificarAutenticacion,getAllOrders)
 
 export default router
