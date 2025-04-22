3a761a288cf466cff0b128ab0d0efc6405b492ef
Mathias Teran <agusmaty23@gmail.com>
2025-04-22 14:20:24 -0500

    feat: agregar validaciones para la creación, obtención, actualización y eliminación de clientes

--- src/routers/clients_routes.js
+++ src/routers/clients_routes.js
@@ -1,13 +1,15 @@
 import {Router} from 'express'
 import { RegisterClient,getAllClients, getClientsById,UpdateClient,DeleteClient } from '../controllers/clients_controller.js'
 import { verificarAutenticacion } from '../middlewares/JWT.js'
+import { validateCreateClient, validateGetClient, validateUpdateClient, validateDeleteClient } from '../validators/clients_validator.js'
+import { validateRequest } from '../middlewares/validator_middleware.js';
 
 const router = Router()
 
-router.post('/clients/register',verificarAutenticacion,RegisterClient)
+router.post('/clients/register',verificarAutenticacion,validateCreateClient,validateRequest,RegisterClient)
 router.get('/clients',verificarAutenticacion,getAllClients )
-router.get('/clients/:ruc',verificarAutenticacion,getClientsById)
-router.patch('/clients/update/:ruc',verificarAutenticacion,UpdateClient)
-router.delete('/clients/delete/:id',verificarAutenticacion,DeleteClient)
+router.get('/clients/:ruc',verificarAutenticacion,validateGetClient,validateRequest,getClientsById)
+router.patch('/clients/update/:ruc',verificarAutenticacion,validateUpdateClient,validateRequest,UpdateClient)
+router.delete('/clients/delete/:id',verificarAutenticacion,validateDeleteClient,validateRequest,DeleteClient)
 
 export default router
---
+++ src/validators/clients_validator.js
@@ -0,0 +1,94 @@
+import {body,param} from 'express-validator'
+import validator from 'ecuador-validator';
+
+export const validateCreateClient = [
+    body('Ruc')
+        .notEmpty().withMessage('El RUC es obligatorio')
+        .custom((value) => {
+            if (!validator.ruc(value)) {
+                throw new Error('El RUC no es válido');
+            }
+            return true;
+        }),
+    body('Name')
+        .notEmpty().withMessage('El nombre es obligatorio')
+        .isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
+    body('Address')
+        .notEmpty().withMessage('La dirección es obligatoria')
+        .isLength({ min: 8,max:200 }).withMessage('La dirección debe tener al menos 8 caracteres maximo 200'),
+    body('telephone')
+        .notEmpty().withMessage('El teléfono es obligatorio')
+        .custom((value) => {
+            if (!validator.cellphone(value,'code')) {
+                throw new Error('El teléfono no es válido');
+            }
+            return true;
+        }),
+    body('email')
+        .notEmpty().withMessage('El correo electrónico es obligatorio')
+        .isEmail().withMessage('El correo electrónico no es válido')
+        .isLength({ max: 100 }).withMessage('El correo electrónico no debe exceder los 100 caracteres'),
+    body('credit')
+        .notEmpty().withMessage('El crédito es obligatorio')
+        .isString().withMessage('El crédito debe ser un texto')
+        .isLength({ min:4, max: 10 }).withMessage('El crédito debe tener entre 4 y 10 caracteres'),
+    body('state')
+        .notEmpty().withMessage('El estado es obligatorio')
+        .isString().withMessage('El estado debe ser un texto')
+        .isIn(['al día', 'en deuda']).withMessage('El estado debe ser "al día" o "en deuda"'),
+]
+
+export const validateGetClient = [
+    param('ruc')
+        .notEmpty().withMessage('El RUC es obligatorio')
+        .custom((value) => {
+            if (!validator.ruc(value)) {
+                throw new Error('El RUC no es válido');
+            }
+            return true;
+        }),
+]
+
+export const validateUpdateClient = [
+    param('ruc')
+        .notEmpty().withMessage('El RUC es obligatorio')
+        .custom((value) => {
+            if (!validator.ruc(value)) {
+                throw new Error('El RUC no es válido');
+            }
+            return true;
+        }),
+    body('Name')
+        .optional()
+        .isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
+    body('Address')
+        .optional()
+        .isLength({ min: 8,max:200 }).withMessage('La dirección debe tener al menos 8 caracteres maximo 200'),
+    body('telephone')
+        .optional()
+        .custom((value) => {
+            if (!validator.cellphone(value,'code')) {
+                throw new Error('El teléfono no es válido');
+            }
+            return true;
+        }),
+    body('email')
+        .optional()
+        .isEmail().withMessage('El correo electrónico no es válido')
+        .isLength({ max: 100 }).withMessage('El correo electrónico no debe exceder los 100 caracteres'),
+    body('credit')
+        .optional()
+        .isString().withMessage('El crédito debe ser un texto')
+        .isLength({ min:4, max: 10 }).withMessage('El crédito debe tener entre 4 y 10 caracteres'),
+    body('state')
+        .optional()
+        .isString().withMessage('El estado debe ser un texto')
+        .isIn(['al día', 'en deuda']).withMessage('El estado debe ser "al día" o "en deuda"'),
+]
+
+export const validateDeleteClient = [
+    param('id')
+        .notEmpty().withMessage('El ID es obligatorio')
+        .isMongoId().withMessage('El ID no es válido')
+]
+
