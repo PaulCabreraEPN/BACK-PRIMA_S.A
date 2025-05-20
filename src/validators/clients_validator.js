e8c3fdb0055627b191525eb6a0d8a9593a91e848
Mathias Teran <agusmaty23@gmail.com>
2025-05-19 19:23:33 -0500

    fix: ajustar validaciones de longitud en los campos Nombre y Nombre Comercial del cliente

--- src/validators/clients_validator.js
+++ src/validators/clients_validator.js
@@ -12,10 +12,10 @@ export const validateCreateClient = [
         }),
     body('Name')
         .notEmpty().withMessage('El nombre es obligatorio')
-        .isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
+        .isLength({ min: 3, max: 25 }).withMessage('El nombre debe tener al menos 3 caracteres y máximo 25'),
     body('ComercialName')
         .notEmpty().withMessage('El nombre comercial es obligatorio')
-        .isLength({ min: 3 }).withMessage('El nombre comercial debe tener al menos 3 caracteres'),
+        .isLength({ min: 3,max:50 }).withMessage('El nombre comercial debe tener al menos 3 caracteres y máximo 50'),
     body('Address')
         .notEmpty().withMessage('La dirección es obligatoria')
         .isLength({ min: 8,max:200 }).withMessage('La dirección debe tener al menos 8 caracteres maximo 200'),
@@ -59,10 +59,10 @@ export const validateUpdateClient = [
         }),
     body('Name')
         .optional()
-        .isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
+        .isLength({ min: 3,max:25 }).withMessage('El nombre debe tener al menos 3 caracteres y máximo 25'),
     body('ComercialName')
         .optional()
-        .isLength({ min: 3 }).withMessage('El nombre comercial debe tener al menos 3 caracteres'),
+        .isLength({ min: 3, max:50 }).withMessage('El nombre comercial debe tener al menos 3 caracteres y máximo 50'),
     body('Address')
         .optional()
         .isLength({ min: 8,max:200 }).withMessage('La dirección debe tener al menos 8 caracteres maximo 200'),
